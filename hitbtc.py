ff4cd0555693df5c0a232de78ed71a68d128998f
FawkesPan <i@fawkex.me>
2018-07-20 08:46:12 +0000

    Bug Fix

--- hitbtc.py
+++ hitbtc.py
@@ -12,9 +12,9 @@ def GetPrice(pair='KINETH'):
     url = 'https://api.hitbtc.com/api/2/public/orderbook/' + pair
     data = requests.get(url).json()
     ret = {}
-    ret['ask'] = float(data['ask'][0]['price']/Fee)
+    ret['ask'] = float(data['ask'][0]['price'])/Fee
     ret['askamount'] = int(data['ask'][0]['size'])
-    ret['bid'] = float(data['bid'][0]['price']*Fee)
+    ret['bid'] = float(data['bid'][0]['price'])*Fee
     ret['bidamount'] = int(data['bid'][0]['size'])
     return ret
 
--- stellar.py
+++ stellar.py
@@ -13,9 +13,9 @@ def GetPrice():
     data = requests.get('https://horizon.stellar.org/order_book?selling_asset_type=credit_alphanum4&selling_asset_code=KIN&selling_asset_issuer=GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR&buying_asset_type=native').json()
     hit = hitbtc.GetPrice('XLMETH')
     ret = {}
-    ret['ask'] = float(float(data['asks'][0]['price'])*hit['bid']/Fee)
+    ret['ask'] = float(float(data['asks'][0]['price'])*hit['bid'])/Fee
     ret['askamount'] = int(float(data['asks'][0]['amount'])/float(data['asks'][0]['price']))
-    ret['bid'] = float(float(data['bids'][0]['price'])*hit['ask']*Fee)
+    ret['bid'] = float(float(data['bids'][0]['price'])*hit['ask'])*Fee
     ret['bidamount'] = int(float(data['bids'][0]['amount'])/float(data['bids'][0]['price']))
     return ret
 
